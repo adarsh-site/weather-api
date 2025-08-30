05c2c1b9e07db99048943a99cd6883544cd9d3d1
adarsh <adarshkumarmsay@gmail.com>
2025-08-30 18:14:22 +0530

    add index.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import "dotenv/config";
+import { rateLimit } from 'express-rate-limit'
+import weatherRoutes from './routes/weatherRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+})
+
+app.use(limiter);
+app.use("/", weatherRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
