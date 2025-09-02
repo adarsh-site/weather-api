ee86b96b042fb961259d35fb3611c0c8566ec70d
adarsh <adarshkumarmsay@gmail.com>
2025-09-02 12:03:02 +0530

    fix createClient function

--- config/redisClient.js
+++ config/redisClient.js
@@ -2,7 +2,12 @@ import { createClient } from "redis";
 import "dotenv/config";
 
 const redisClient = createClient({
-  url: `redis://${process.env.REDIS_HOST || "127.0.0.1"}:${process.env.REDIS_PORT || 6379}`,
+  username: process.env.REDIS_USERNAME || "default",
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
 });
 
 redisClient.on("error", (err) => console.error("❌ Redis Client Error:", err));
--- server.js
+++ server.js
@@ -1,21 +1,34 @@
-import express from 'express';
+import express from "express";
 import "dotenv/config";
-import rateLimit from 'express-rate-limit'
-import weatherRoutes from './routes/weatherRoutes.js';
+import rateLimit from "express-rate-limit";
+import weatherRoutes from "./routes/weatherRoutes.js";
+import redisClient from "./config/redisClient.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    limit: 100,
-    standardHeaders: true,
-    legacyHeaders: false,
-})
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 app.use(limiter);
 app.use("/", weatherRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+const startServer = async () => {
+  try {
+    await redisClient.connect();
+    console.log("Connected to Redis");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Failed to connect to Redis:", err);
+    process.exit(1);
+  }
+};
+
+startServer();
