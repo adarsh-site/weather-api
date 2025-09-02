f66d61ca77d3bc34c3e630db5990afd7791308c1
adarsh <adarshkumarmsay@gmail.com>
2025-09-02 11:49:02 +0530

    fix redisClient config

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,8 +1,10 @@
 import { createClient } from "redis";
-import "dotenv/config()";
+import "dotenv/config";
 
-export const redisClient = await createClient({
-  url: `redis://${process.env.REDIS_HOST || '127.0.0.1'}:${process.env.REDIS_PORT || 6379}`,
-})
-  .on("error", (err) => console.log("Redis Client Error", err))
-  .connect();
+const redisClient = createClient({
+  url: `redis://${process.env.REDIS_HOST || "127.0.0.1"}:${process.env.REDIS_PORT || 6379}`,
+});
+
+redisClient.on("error", (err) => console.error("❌ Redis Client Error:", err));
+
+export default redisClient;
