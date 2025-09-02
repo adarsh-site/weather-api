007bf4a0bad2eb27cb4912f0138c68fe9f61627a
adarsh <adarshkumarmsay@gmail.com>
2025-09-02 10:33:27 +0530

    add weatherRoutes.js

---
+++ routes/weatherRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import {getWeather} from '../controllers/weatherController.js';
+
+const router = express.Router();
+
+router.get('/weather/:city', getWeather);
+
+export default router;
