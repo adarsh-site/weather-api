3404b7f97f48b263cf225773c99e26e208180d71
adarsh <adarshkumarmsay@gmail.com>
2025-09-02 11:42:52 +0530

    add rate limit checks

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import "dotenv/config";
-import { rateLimit } from 'express-rate-limit'
+import rateLimit from 'express-rate-limit'
 import weatherRoutes from './routes/weatherRoutes.js';
 
 const app = express();
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3000;
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     limit: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
 })
 
 app.use(limiter);
